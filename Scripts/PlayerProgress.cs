43ec402a50e9446e13c8e5cec8b4585a4e488b17
Victor <67949369+Duchmolle@users.noreply.github.com>
2021-04-12 11:14:38 +0200

    Add files via upload

---
+++ Scripts/PlayerProgress.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerProgress : MonoBehaviour
+{
+    [SerializeField] QuestZone _jimmyInteractZone, _margueriteInteractZone, _dogInteractZone;
+    [SerializeField] SanityManager _sanity;
+    [SerializeField] AudioClip _pickupSFX;
+    private AudioSource _sound;
+    public List<Collectible> _itemsObtained;
+
+    void Start()
+    {
+        _sound = GetComponent<AudioSource>();
+        _sound.clip = _pickupSFX;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public int SanityLevel()
+    {
+        return _sanity._sanityLevel;
+    }
+
+    public void AddItem(Collectible newItem)
+    {
+        _sound.Play();
+        _itemsObtained.Add(newItem);
+        _sanity.IncreaseSanity();
+    }
+
+    public void RemoveItem(int itemNumber)
+    {
+        for(int i = 0; i < _itemsObtained.Count ; i++ )
+        {
+            if(_itemsObtained[i]._questNumber == itemNumber)
+            {
+                _itemsObtained.Remove(_itemsObtained[i]);
+                Debug.Log("item Removed");
+            }
+        }
+
+    }
+
+    public bool HasItem(int itemNumber)
+    {
+        foreach (Collectible item in _itemsObtained)
+        {
+            if (item._questNumber == itemNumber)
+            {
+                return true;
+            }
+            else return false;
+        }
+        return false;
+    }
+}
---
+++ Scripts/QuestZone.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class QuestZone : MonoBehaviour
+{
+    [SerializeField] int _questNumber;
+    [SerializeField] PlayerProgress _player;
+    [SerializeField] GameObject _targetObject;
+    [SerializeField] AudioClip _questSFX;
+    [SerializeField] TMP_Text _questText;
+    private bool _questDone;
+    private bool _textAvailable = true;
+    private AudioSource _sound;
+
+    private void Start()
+    {
+        _sound = GetComponent<AudioSource>();
+        _sound.clip = _questSFX;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (_player.HasItem(_questNumber) && !_questDone)
+        {
+            Debug.Log("Quest DONE !");
+            _player.RemoveItem(_questNumber);
+            _sound.Play();
+            _questText.gameObject.SetActive(false);
+            if (_player.SanityLevel() >= 3)
+            {
+                _targetObject.SetActive(true);
+                StartCoroutine(Outro(3));
+            }
+            //SON DE QUETE ACCOMPLIE
+        }
+        else ShowQuestLine();
+        Debug.Log("Get the item !");
+    }
+
+    private void ShowQuestLine()
+    {
+        if (_textAvailable)
+        {
+            StartCoroutine(TextCooldown());
+        }
+    }
+
+    private IEnumerator TextCooldown()
+    {
+        _textAvailable = false;
+        yield return new WaitForSeconds(5);
+        _textAvailable = true;
+    }
+
+    private IEnumerator Outro(float time)
+    {
+        FindObjectOfType<QuestItemInteraction>().Fade();
+        yield return new WaitForSeconds(time);
+        FindObjectOfType<SceneLoader>().LoadNextScene();
+    }
+}
